821e433168290c6b9f333c6fd51b804b98657a63
Philip Wagner <filip.sound@gmail.com>
2018-07-23 01:45:10 +0200

    palette simplification

--- T2Tools/Formats/Palette.cs
+++ T2Tools/Formats/Palette.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace T2Tools.Formats
 {
@@ -14,11 +13,7 @@ namespace T2Tools.Formats
                 for (int x = 0; x < bmp.Width; x++)
                 {
                     int i = x + y * bmp.Width;
-                    byte r = (byte)Math.Floor(data[i] * 255f / 63f);
-                    byte g = (byte)Math.Floor(data[i + 1] * 255f / 63f);
-                    byte b = (byte)Math.Floor(data[i + 2] * 255f / 63f);
-
-                    Color color = Color.FromArgb(255, r, g, b);
+                    Color color = Color.FromArgb(255, 4*data[i], 4 * data[i + 1], 4 * data[i + 2]);
                     bmp.SetPixel(x, y, color);
                 }
             }
